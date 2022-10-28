3195c919321b4c752d29f0472d62438e451ece34
tunzira <116047979+tunzira@users.noreply.github.com>
2022-10-28 16:37:29 +0700

    Base gameplay
    Gamepong

---
+++ Assets/Script/PedalContorller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PedalContorller : MonoBehaviour
+{
+    public int speed;
+    public KeyCode upkey;
+    public KeyCode downkey;
+
+    private Rigidbody2D rig;
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        MoveObject(GetInput());
+    }
+
+    private Vector2 GetInput()
+    {
+        if (Input.GetKey(upkey))
+        {
+            return Vector2.up *  speed;
+        }
+        else if (Input.GetKey(downkey))
+        {
+            return Vector2.down * speed;
+        }
+        return Vector2.zero;
+    }
+    private void MoveObject(Vector2 movement) 
+    {
+        rig.velocity = movement;
+    }
+}
